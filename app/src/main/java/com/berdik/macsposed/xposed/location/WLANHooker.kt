eea1b3afccf65d38727f5694b2d1d78ffca821c1
David Berdik <berdikgit@theprogrammersworld.net>
2022-05-05 17:14:01 -0400

    More refactoring and experimenting.

--- app/src/main/java/com/berdik/macsposed/xposed/location/WLANHooker.kt
+++ app/src/main/java/com/berdik/macsposed/xposed/location/WLANHooker.kt
@@ -1,13 +1,14 @@
 package com.berdik.macsposed.xposed.location
 
 import android.annotation.SuppressLint
-import android.net.wifi.ScanResult
-import android.net.wifi.WifiInfo
+import android.net.MacAddress
+import android.net.wifi.WifiConfiguration
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.github.kyuubiran.ezxhelper.utils.*
 import dalvik.system.PathClassLoader
 import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class WLANHooker {
@@ -16,60 +17,29 @@ class WLANHooker {
     @SuppressLint("PrivateApi")
     fun hookWifiManager(lpparam: XC_LoadPackage.LoadPackageParam) {
         val clazz: Class<*> = lpparam.classLoader.loadClass("com.android.server.SystemServiceManager")
+        XposedBridge.log("Searching for System Service Manager.")
         findAllMethods(clazz) {
             name == "loadClassFromLoader" && isPrivate && isStatic
         }.hookMethod {
             after { param ->
-                XposedBridge.log("FL: in loadClassFromLoader with service: " + param.args[0])
                 if (param.args[0] == "com.android.server.wifi.WifiService") {
-                    XposedBridge.log("FL: Awesome! Now we are finding the REAL method...")
+                    XposedBridge.log("System Service Manager found! Searching for WiFi Service.")
                     try {
                         val classloader = param.args[1] as PathClassLoader
-                        val wifiClazz = classloader.loadClass("com.android.server.wifi.WifiServiceImpl")
+                        val wifiClazz = classloader.loadClass("com.android.server.wifi.WifiConfigManager")
 
                         findAllMethods(wifiClazz) {
-                            name == "getScanResults" && isPublic
+                            name == "getInternalConfiguredNetwork" && isPrivate
                         }.hookMethod {
                             after { param ->
-                                val packageName = param.args[0] as String
-                                XposedBridge.log("FL: In getScanResults with caller: $packageName")
-
-                                XposedBridge.log("FL: in whitelist! Return custom WiFi information")
-
-                                val customResult = ScanResult()
-                                customResult.BSSID = ""
-                                customResult.SSID = "AndroidAP"
-                                customResult.capabilities = "WPA-2"
-                                customResult.level = -1
-
-                                val result: List<ScanResult> = listOf()
-                                param.result = result
-
-                                XposedBridge.log("FL: BSSID: ${customResult.BSSID}, SSID: ${customResult.SSID}")
+                                if (param.result != null) {
+                                    val mac = MacAddress.fromString("69:69:69:69:69:69");
+                                    XposedHelpers.setObjectField(param.result, "mRandomizedMacAddress", mac)
+                                    val config = param.result as WifiConfiguration
+                                    XposedBridge.log("SSID is ${config.SSID}, MAC is ${config.randomizedMacAddress}")
+                                }
                             }
                         }
-
-                        findAllMethods(wifiClazz) {
-                            name == "getConnectionInfo" && isPublic
-                        }.hookMethod {
-                            after { param ->
-                                val packageName = param.args[0] as String
-                                XposedBridge.log("FL: In getConnectionInfo with caller: $packageName")
-
-                                XposedBridge.log("FL: in whitelist! Return custom WiFi information")
-
-                                val customResult = WifiInfo.Builder()
-                                    .setBssid("")
-                                    .setSsid("Android-AP".toByteArray())
-                                    .setRssi(-1)
-                                    .setNetworkId(0)
-                                    .build()
-
-                                param.result = customResult
-                                XposedBridge.log("FL: BSSID: ${customResult.bssid}, SSID: ${customResult.ssid}")
-                            }
-                        }
-
                     } catch (e: Exception) {
                         XposedBridge.log("FL: fuck with exceptions! $e")
                     }
