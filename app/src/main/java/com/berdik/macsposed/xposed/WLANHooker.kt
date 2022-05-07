501f2999dc4f65c79bcd8de71747b824409c959a
David Berdik <berdikgit@theprogrammersworld.net>
2022-05-07 19:18:49 -0400

    Found a much simpler and more reliable way to do it!

--- app/src/main/java/com/berdik/macsposed/xposed/WLANHooker.kt
+++ app/src/main/java/com/berdik/macsposed/xposed/WLANHooker.kt
@@ -7,7 +7,9 @@ import androidx.annotation.RequiresApi
 import com.github.kyuubiran.ezxhelper.utils.*
 import dalvik.system.PathClassLoader
 import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import java.net.NetworkInterface
 
 class WLANHooker {
     @RequiresApi(Build.VERSION_CODES.R)
@@ -23,28 +25,15 @@ class WLANHooker {
                     try {
                         val classloader = param.args[1] as PathClassLoader
 
-                        // Step 1
-                        val wifiConfigManagerClass = classloader.loadClass("com.android.server.wifi.WifiConfigManager")
-                        findAllMethods(wifiConfigManagerClass) {
-                            name == "shouldUseEnhancedRandomization" && isPublic
-                        }.hookReplace {
-                            false
-                        }
-
-                        // Step 2
-                        findAllMethods(wifiConfigManagerClass) {
-                            name == "getPersistentMacAddress" && isPublic
-                        }.hookReplace {
-                            MacAddress.fromString("12:34:56:78:90:ab")
-                            //MacAddress.fromString("50:1a:c5:cf:9f:8e") - Microsoft MAC
-                        }
-
-                        // Step 3
-                        val wifiConfigClass = lpparam.classLoader.loadClass("android.net.wifi.WifiConfiguration")
-                        findAllMethods(wifiConfigClass) {
-                            name == "isValidMacAddressForRandomization" && isPublic
-                        }.hookReplace {
-                            true
+                        // Step 5
+                        val wifiVendorHalClass = classloader.loadClass("com.android.server.wifi.WifiVendorHal")
+                        findAllMethods(wifiVendorHalClass) {
+                            name == "setStaMacAddress" && isPublic
+                        }.hookMethod {
+                            before { param ->
+                                XposedBridge.log("BEFORE setStaMacAddress: ${MacAddress.fromBytes(NetworkInterface.getByName("wlan0").hardwareAddress)}")
+                                param.result = true
+                            }
                         }
                     } catch (e: Exception) {
                         XposedBridge.log("MACsposed Error: $e")
@@ -56,15 +45,7 @@ class WLANHooker {
 }
 
 /*
-    Step 1:
+    Step 5:
     https://cs.android.com/android/platform/superproject/+/master:packages/modules/Wifi/service/java/com/android/
-        server/wifi/WifiConfigManager.java;drc=5e87eff15a543aa27ecaf00f41b3edc47de3fd6a;l=428
-
-    Step 2:
-    https://cs.android.com/android/platform/superproject/+/master:packages/modules/Wifi/service/java/com/android/
-        server/wifi/WifiConfigManager.java;drc=5e87eff15a543aa27ecaf00f41b3edc47de3fd6a;l=505
-
-    Step 3:
-    https://cs.android.com/android/platform/superproject/+/master:packages/modules/Wifi/framework/java/android/
-        net/wifi/WifiConfiguration.java;drc=5e87eff15a543aa27ecaf00f41b3edc47de3fd6a;l=1715
+        server/wifi/WifiVendorHal.java;drc=8edc7df508e04e982b9ef715d2f48e5cb3b1478e;l=1730
  */
