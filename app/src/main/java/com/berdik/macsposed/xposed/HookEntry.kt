8a83405adbd2daea5093b917cc9529e6d1ee36b2
David Berdik <berdikgit@theprogrammersworld.net>
2022-05-07 20:45:42 -0400

    More refactoring.

--- app/src/main/java/com/berdik/macsposed/xposed/HookEntry.kt
+++ app/src/main/java/com/berdik/macsposed/xposed/HookEntry.kt
@@ -2,20 +2,18 @@ package com.berdik.macsposed.xposed
 
 import android.annotation.SuppressLint
 import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
+import com.github.kyuubiran.ezxhelper.utils.*
+import dalvik.system.PathClassLoader
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.IXposedHookZygoteInit
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
-import java.lang.Exception
-
-@ExperimentalStdlibApi
 class HookEntry : IXposedHookZygoteInit, IXposedHookLoadPackage {
     override fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
         EzXHelperInit.initZygote(startupParam)
     }
 
-    @SuppressLint("PrivateApi", "ObsoleteSdkInt", "NewApi")
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
         if (lpparam != null) {
             when (lpparam.packageName) {
@@ -25,13 +23,34 @@ class HookEntry : IXposedHookZygoteInit, IXposedHookLoadPackage {
                     EzXHelperInit.setToastTag("MACsposed")
 
                     try {
-                        WLANHooker().hookWifiManager(lpparam)
+                        hookWifiService(lpparam)
                     } catch (e: Exception) {
                         XposedBridge.log("MACsposed Error: $e")
                     }
                 }
             }
         }
+    }
 
+    @SuppressLint("PrivateApi")
+    private fun hookWifiService(lpparam: XC_LoadPackage.LoadPackageParam) {
+        findAllMethods(lpparam.classLoader.loadClass("com.android.server.SystemServiceManager")) {
+            name == "loadClassFromLoader" && isPrivate && isStatic
+        }.hookMethod {
+            after { param ->
+                if (param.args[0] == "com.android.server.wifi.WifiService") {
+                    hookMacAddrSet(param.args[1] as PathClassLoader)
+                }
+            }
+        }
+    }
+
+    @SuppressLint("PrivateApi")
+    private fun hookMacAddrSet(classloader: PathClassLoader) {
+        findAllMethods(classloader.loadClass("com.android.server.wifi.WifiVendorHal")) {
+            name == "setStaMacAddress" && isPublic
+        }.hookReplace {
+            true
+        }
     }
 }
