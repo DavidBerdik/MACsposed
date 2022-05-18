4d2e1d9cba6f05daa90304f1ef7929a66e5e9f17
David Berdik <berdikgit@theprogrammersworld.net>
2022-05-18 13:03:43 -0400

    We now automatically add the tile to the end of the quick settings panel! Still need to refine it so that it is revealed, though.

--- app/src/main/java/com/berdik/macsposed/hookers/SystemUIHooker.kt
+++ app/src/main/java/com/berdik/macsposed/hookers/SystemUIHooker.kt
@@ -1,10 +1,9 @@
 package com.berdik.macsposed.hookers
 
 import android.annotation.SuppressLint
-import com.github.kyuubiran.ezxhelper.utils.findAllMethods
-import com.github.kyuubiran.ezxhelper.utils.hookMethod
-import com.github.kyuubiran.ezxhelper.utils.isPublic
+import com.github.kyuubiran.ezxhelper.utils.*
 import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class SystemUIHooker {
@@ -15,8 +14,27 @@ class SystemUIHooker {
                 name == "setRevealExpansion" && isPublic
             }.hookMethod {
                 before { param ->
-                    if (param.args[0] == 1f)
+                    if (param.args[0] == 1f) {
+                        val tileHost = XposedHelpers.getObjectField(param.thisObject, "mHost")
+                        XposedHelpers.callMethod(tileHost, "addTile", "custom(com.berdik.macsposed/.QuickTile)", -1)
                         XposedBridge.log("[MACsposed] PANEL OPENED!")
+                    }
+                }
+            }
+
+            findAllMethods(lpparam.classLoader.loadClass("com.android.systemui.qs.QSTileHost")) {
+                name == "addTile" && isPublic && paramCount == 2
+            }.hookMethod {
+                before { param ->
+                    XposedBridge.log("[MACsposed] addTile CALLED - ${param.args[0]}")
+                }
+            }
+
+            findAllMethods(lpparam.classLoader.loadClass("com.android.systemui.qs.QSTileHost")) {
+                name == "changeTiles" && isPublic && paramCount == 2
+            }.hookMethod {
+                before { param ->
+                    XposedBridge.log("[MACsposed] changeTiles CALLED - ${param.args[0]} | ${param.args[1]}")
                 }
             }
         }
